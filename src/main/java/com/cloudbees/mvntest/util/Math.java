0d113a067c2c35b9bad95143189fa79b74ba5f3f
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-01 11:27:20 -0500

    5th param

--- src/main/java/com/cloudbees/mvntest/util/Math.java
+++ src/main/java/com/cloudbees/mvntest/util/Math.java
@@ -2,7 +2,7 @@ package com.cloudbees.mvntest.util;
 
 public class Math {
 
-    public static int add (int a, int b, int c, int d) {
+    public static int add (int a, int b, int c, int d, int e) {
 	return a + b + c + d;
     }
 

a5953c40c02329799bfbedcdafa8e06c5efec986
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-15 09:57:27 -0500

    simplify

--- src/main/java/com/cloudbees/mvntest/util/Math.java
+++ src/main/java/com/cloudbees/mvntest/util/Math.java
@@ -2,8 +2,8 @@ package com.cloudbees.mvntest.util;
 
 public class Math {
 
-    public static int add (int a, int b, int c, int d, int e) {
-	return a + b + c + d;
+    public static int add (int a, int b) {
+	return a + b;
     }
 
     public static int subtract (int a, int b) {
